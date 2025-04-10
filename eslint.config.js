41196c1ab2b8ada77a939bed98d5c21e6156bac4
albertsarifullin <alba9211@mail.ru>
2025-04-10 14:03:05 +0300

    10.04: добавил prettier eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      // Добавляем конфиг Prettier (отключает конфликтующие правила ESLint)
+      prettierConfig,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +23,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      // Добавляем плагин Prettier (включает правила Prettier в ESLint)
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +32,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Включаем правило Prettier для форматирования
+      'prettier/prettier': 'error',
     },
   },
-)
+);
